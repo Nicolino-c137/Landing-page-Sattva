8b3493817a1a0f90ac1379446896e59165420a7d
Nicolino <nico262626las@gmail.com>
2026-05-05 18:09:07 -0300

    Terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  base: 'sattvaSpa',
+  base: '/sattvaSpa',
   vite: {
     plugins: [tailwindcss()],
   },
